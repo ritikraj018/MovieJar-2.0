f140a04a10e8310c62328249537a4589aa90be31
Ritik Raj <ritikrajbarh18@gmail.com>
2023-12-08 23:42:11 +0530

    Components and Routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+
+import { Movies, MovieInformation, Actors, Profile, NavBar } from '.';
+
 const App = () => (
-  <div>App</div>
+  <div>
+    <CssBaseline />
+    <NavBar />
+    <Routes>
+      <Route path="/movie/:id" element={<MovieInformation />} />
+      <Route path="/actors/:id" element={<Actors />} />
+      <Route path="/profile/:id" element={<Profile />} />
+      <Route path="/" element={<Movies />} />
+    </Routes>
+  </div>
 );
 
 export default App;
